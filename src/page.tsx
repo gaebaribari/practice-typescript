8be1533a197af3337d6e117fda58a44cc9df2a2b
gaebaribari <gaebaribari@gmail.com>
2025-02-01 16:01:19 +0900

    Practice tailwind directives

--- src/page.tsx
+++ src/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
     return (
         <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
-           <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-4">
+           <div className="box flex flex-col gap-4">
            <div className="group flex flex-col">
           <input
             className="bg-gray-100 w-full"
@@ -10,7 +10,7 @@ export default function Page() {
           <span className="group-focus-within:block hidden">
             Make sure it is a valid email...
           </span>
-          <button>Submit</button>
+          <button className="bg-[#d6e9ca]">Submit</button>
         </div>
       </div>
         </main>
