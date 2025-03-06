378fd52ec4d4ef2a2738977ab203d5ff72d22a4f
gaebaribari <gaebaribari@gmail.com>
2025-03-06 14:09:54 +0900

    Practice:Recoil

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { useRecoilValue, useSetRecoilState } from "recoil";
 import Page from "./page";
+import { isDarkAtom } from "./atoms";
 
 export default function App() {
-
+  const isDark = useRecoilValue(isDarkAtom);
+const setDarkAtom = useSetRecoilState(isDarkAtom);
   return (
-    <Page></Page>
+    <div>
+      <p>지금 다크모드의 상태는 {String(isDark)}입니다 </p>
+      <button onClick={()=>setDarkAtom((prev)=>!prev)}>다크모드 상태바꾸기</button>
+      <Page></Page>
+    </div>
   );
 }
